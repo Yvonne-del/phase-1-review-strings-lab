4a9d70bd396bec29d02fa89f8e580acfa31e1521
Yvonne-del <ys359375@gmail.com>
2025-03-06 08:45:33 +0300

    create and manipulate string values

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Write your code in this file!
+const currentUser = "Yvy";
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+const excitedWelcomeMessage = welcomeMessage.toLocaleUpperCase();
+const shortGreeting = `Welcome, ${currentUser[0]}!`;
+console.log(currentUser.slice(1));
